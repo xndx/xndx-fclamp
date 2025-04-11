d5489c54bfe1e92f7a016f1a269c49f366d03884
xndx <m@xndx.dk>
2025-04-11 10:58:12 +0200

    update

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+      </head>
       <body>{children}</body>
     </html>
   )
